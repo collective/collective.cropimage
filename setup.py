0dd30856754ee1cb0775f59e7edf82915483543c
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-07-16 16:05:50 +0300

    release 1.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='collective.cropimage',
-    version='1.7',
+    version='1.8',
     description="Store cropped image dimension for farther usage.",
     long_description=long_description,
     classifiers=[
